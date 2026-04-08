b89bca0290c110f984548fbb6b9e29572e0cbc8a
@skye <sonicgames772@gmail.com>
2026-04-08 17:47:21 -0300

    eu tb amo javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+const elements = document.querySelectorAll('.fade-in');
+
+window.addEventListener('scroll', () => {
+  elements.forEach(el => {
+    const position = el.getBoundingClientRect().top;
+    const screen = window.innerHeight;
+
+    if (position < screen - 100) {
+      el.classList.add('show');
+    }
+  });
+});
