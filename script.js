8cf2c3f38b9c419dc498953d903a26ff7db46c73
@skye <sonicgames772@gmail.com>
2026-04-08 20:07:28 -0300

    hamburguer

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Site Luiz Gonzaga carregado com sucesso!");
+
+/* hamburguer */
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
