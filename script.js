0e2ff9f8106bd34d8728f1911f52713a2a175ee5
@skye <sonicgames772@gmail.com>
2026-04-08 18:33:48 -0300

    a

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-// Fade-in ao scroll
-const elements = document.querySelectorAll('.fade-in');
-window.addEventListener('scroll', () => {
-  elements.forEach(el => {
-    if(el.getBoundingClientRect().top < window.innerHeight - 100){
-      el.classList.add('show');
-    }
-  });
-});
-
-// Áudio
-function playAudio(id){
-  const audio = document.getElementById(id);
-  if(audio){ audio.currentTime=0; audio.play(); }
-}
+console.log("Site Luiz Gonzaga carregado com sucesso!");
