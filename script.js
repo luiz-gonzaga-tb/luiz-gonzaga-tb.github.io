d514b6aa6be88b033783b1e5b6ed1c922c2374ca
@skye <sonicgames772@gmail.com>
2026-04-08 17:59:43 -0300

    toma js

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
+// FADE IN
 const elements = document.querySelectorAll('.fade-in');
+
+window.addEventListener('scroll', () => {
+  elements.forEach(el => {
+    const position = el.getBoundingClientRect().top;
+    const screen = window.innerHeight;
+
+    if (position < screen - 100) {
+      el.classList.add('show');
+    }
+  });
+});
+
+// ÁUDIO
+function playAudio(id) {
+  const audio = document.getElementById(id);
+  if(audio) {
+    audio.currentTime = 0;
+    audio.play();
+  }
+}
+
+// MODO ESCURO
+const darkModeBtn = document.getElementById('darkModeBtn');
+darkModeBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+});const elements = document.querySelectorAll('.fade-in');
 
 window.addEventListener('scroll', () => {
   elements.forEach(el => {
